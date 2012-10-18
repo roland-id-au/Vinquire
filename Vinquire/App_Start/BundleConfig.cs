86a07d3a542505fe58891dcadadb56abfeb982a3
Blake Carter <blake@roland.id.au>
2012-10-19 03:10:56 +1000

    Initial commit

---
+++ Vinquire/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Vinquire
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+            
+            var scripts = new ScriptBundle("~/js").Include(
+                "~/Content/themes/base/js/jquery-*",
+                "~/Content/themes/base/js/bootstrap.js*",
+                "~/Content/js/vin.js"
+            );
+            //scripts.Transforms.Add(new JsMinify());
+
+            bundles.Add(scripts);
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.unobtrusive*",
+            //            "~/Scripts/jquery.validate*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            var styles = new StyleBundle("~/styles").Include("~/Content/site.less");
+            styles.Transforms.Add(new LessMinify());
+            styles.Transforms.Add(new CssMinify());
+
+            bundles.Add(styles);
+        }
+    }
+}
---
+++ Vinquire/Assets/scripts/vin.js
@@ -0,0 +1,99 @@
+﻿!function ($) {
+    $.extend(true, $.fn.popover.Constructor.prototype, {
+        update: function () {
+            if (arguments.length == 1 && typeof (arguments[0]) == 'string')
+                this.options.content = arguments[0];
+
+            var $tip = this.tip(), content = this.getContent();
+
+            $tip.find('.popover-content > *')[this.options.html ? 'html' : 'text'](content)
+
+            //$tip.removeClass('fade top bottom left right in')
+        }
+    });
+}(window.jQuery);
+
+
+
+
+var invalidChars = /[^A-z0-9]/;
+var blockChars = /[iIoOQq ]/;
+
+$(document).ready(function () {
+    //$("#vin").mask("vvv-vvvvvv-vvvvvvvv");
+    $('#vin').bind('keydown', function(e){
+        var key = e.keyCode | e.charCode;
+
+        if (
+            (key == 32) ||
+            (key >= 48 && key <= 57 && event.shiftKey) || 
+            (key >= 91 && key <= 93) || // Alphanumeric
+            (key > 106)
+        ) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    });
+
+    $('#vin').bind('propertychange keyup paste', function (e) {
+        var el = $(this);
+        var wrapper = el.parent();
+        var old = el.data('old');
+        var value = el.val().replace(/[^\w]/g, '');
+        var hasPopover = wrapper.data('popover') != null || false;
+        var formattedValue = '';
+
+        //for (var i = 0; i < value.length; i++) {
+        //    formattedValue += value.charAt(i);
+            
+        //    if ((i == 2 || i == 8) && i < value.length - 1)
+        //        formattedValue += '-';
+        //}
+
+        //if (old != formattedValue && && ) {
+        //    el.data('old', formattedValue);
+        //    el.val(formattedValue);
+        //}
+
+        if (invalidChars.test(value) || blockChars.test(value)) {
+            var content = '';
+            content += 'Try checking the letters highligted below to make sure they exactly match your <i>VIN</i>.';
+            content += '<div class="vin-display">';
+            content += '<ul>';
+
+            for(var i = 0; i < value.length; i++){
+                var char = value.charAt(i);
+
+                if (invalidChars.test(char) || blockChars.test(char)) {
+                    content += '<li class="vin-display-char invalid">' + char + '</li>';
+                } else {
+                    content += '<li class="vin-display-char">' + char + '</li>';
+                }
+            }
+
+            content += '</ul>';
+            content += '</div>';
+
+            if (!hasPopover) {
+                wrapper.popover({
+                    animation: true,
+                    placement: 'inside bottom',
+                    trigger: 'manual',
+                    title: 'Hmm, that doesn\'t seem right...',
+                    //content: function () { return content; }
+                    content: content
+                });
+
+                wrapper.popover('show');
+
+            } else {
+                wrapper.data('popover').update(content);
+            }
+
+        } else {
+            if (hasPopover) {
+                wrapper.popover('destroy')
+            }
+        }
+    });
+});
